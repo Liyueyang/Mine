c3858e3e9f58c5e04be189011bea69624fa0c3b3
Yueyang_Li <hnhbliyueyang@163.com>
2016-07-06 00:12:47 +0800

    commit:change all logic

--- Demo/FindYou/code/FindYou/app/src/main/java/cn/lyy/findyou/location/LocationManager.java
+++ Demo/FindYou/code/FindYou/app/src/main/java/cn/lyy/findyou/location/LocationManager.java
@@ -2,18 +2,12 @@ package cn.lyy.findyou.location;
 
 import android.content.Context;
 import android.os.Handler;
-import android.util.Log;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.baidu.location.Poi;
 
-import java.lang.ref.WeakReference;
-import java.util.List;
-
-import cn.lyy.findyou.core.Action;
 import cn.lyy.findyou.utils.LocationConfig;
 
 /**
@@ -59,51 +53,51 @@ public class LocationManager implements BDLocationListener {
     @Override
     public void onReceiveLocation(BDLocation location) {
         //Receive Location
-        StringBuffer sb;
-        sb = new StringBuffer(256);
-        sb.append("纬度 : ");
-        sb.append(location.getLatitude());
-        sb.append("\n经度 : ");
-        sb.append(location.getLongitude());
-        sb.append("\n定位精度 : ");
-        sb.append(location.getRadius());
-        if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
-            sb.append("\n速度 : ");
-            sb.append(location.getSpeed());// 单位：公里每小时
-            sb.append("\nGPS锁定用的卫星数 : ");
-            sb.append(location.getSatelliteNumber());
-            sb.append("\n高度 : ");
-            sb.append(location.getAltitude());// 单位：米
-            sb.append("\n行进的方向 : ");
-            sb.append(location.getDirection());// 单位度
-            sb.append("\n详细地址信息 : ");
-            sb.append(location.getAddrStr());
-            sb.append("\n定位方式 : ");
-            sb.append("gps定位成功");
-
-        } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
-            sb.append("\n详细地址信息 : ");
-            sb.append(location.getAddrStr());
-            //运营商信息
-            sb.append("\n运营商信息 : ");
-            sb.append(location.getOperators());
-            sb.append("\n定位方式 : ");
-            sb.append("网络定位成功");
-        } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-            sb.append("\n定位方式 : ");
-            sb.append("离线定位成功，离线定位结果也是有效的");
-        } else if (location.getLocType() == BDLocation.TypeServerError) {
-            sb.append("\n定位方式 : ");
-            sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-        } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-            sb.append("\n定位方式 : ");
-            sb.append("网络不同导致定位失败，请检查网络是否通畅");
-        } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-            sb.append("\n定位方式 : ");
-            sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
-        }
-        sb.append("\n位置语义化信息 : ");
-        sb.append(location.getLocationDescribe());// 位置语义化信息
+//        StringBuffer sb;
+//        sb = new StringBuffer(256);
+//        sb.append("纬度 : ");
+//        sb.append(location.getLatitude());
+//        sb.append("\n经度 : ");
+//        sb.append(location.getLongitude());
+//        sb.append("\n定位精度 : ");
+//        sb.append(location.getRadius());
+//        if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+//            sb.append("\n速度 : ");
+//            sb.append(location.getSpeed());// 单位：公里每小时
+//            sb.append("\nGPS锁定用的卫星数 : ");
+//            sb.append(location.getSatelliteNumber());
+//            sb.append("\n高度 : ");
+//            sb.append(location.getAltitude());// 单位：米
+//            sb.append("\n行进的方向 : ");
+//            sb.append(location.getDirection());// 单位度
+//            sb.append("\n详细地址信息 : ");
+//            sb.append(location.getAddrStr());
+//            sb.append("\n定位方式 : ");
+//            sb.append("gps定位成功");
+//
+//        } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+//            sb.append("\n详细地址信息 : ");
+//            sb.append(location.getAddrStr());
+//            //运营商信息
+//            sb.append("\n运营商信息 : ");
+//            sb.append(location.getOperators());
+//            sb.append("\n定位方式 : ");
+//            sb.append("网络定位成功");
+//        } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+//            sb.append("\n定位方式 : ");
+//            sb.append("离线定位成功，离线定位结果也是有效的");
+//        } else if (location.getLocType() == BDLocation.TypeServerError) {
+//            sb.append("\n定位方式 : ");
+//            sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+//        } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+//            sb.append("\n定位方式 : ");
+//            sb.append("网络不同导致定位失败，请检查网络是否通畅");
+//        } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+//            sb.append("\n定位方式 : ");
+//            sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+//        }
+//        sb.append("\n位置语义化信息 : ");
+//        sb.append(location.getLocationDescribe());// 位置语义化信息
 //        List<Poi> list = location.getPoiList();// POI数据
 //        if (list != null) {
 //            for (Poi p : list) {
@@ -111,9 +105,9 @@ public class LocationManager implements BDLocationListener {
 //                sb.append(p.getName() + " " + p.getRank());
 //            }
 //        }
-        Log.i("LocationManager", sb.toString());
+//        Log.i("LocationManager", sb.toString());
         stop();
-        mHandler.obtainMessage(1, sb.toString()).sendToTarget();
+        mHandler.obtainMessage(1, location).sendToTarget();
     }
 
     private void initLocation() {
--- Demo/FindYou/code/FindYou/app/src/main/java/cn/lyy/findyou/service/LocationService.java
+++ Demo/FindYou/code/FindYou/app/src/main/java/cn/lyy/findyou/service/LocationService.java
@@ -5,13 +5,17 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.SystemClock;
 import android.util.Log;
 
+import com.avos.avoscloud.AVObject;
+
 import cn.lyy.findyou.consts.Consts;
-import cn.lyy.findyou.location.LocationHandler;
 import cn.lyy.findyou.location.LocationManager;
 import cn.lyy.findyou.receiver.StartLocationServiceReceiver;
 
@@ -23,6 +27,8 @@ public class LocationService extends Service {
     private AlarmManager mAlarmManager;
     private Intent mStartServiceIntent;
     private PendingIntent mPi;
+    private StartLocationServiceReceiver mReceiver;
+    private LocationHandler mLocationHandler;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -32,6 +38,11 @@ public class LocationService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        mLocationHandler = new LocationHandler(this);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_USER_PRESENT);
+        mReceiver = new StartLocationServiceReceiver();
+        registerReceiver(mReceiver, intentFilter);
     }
 
     @Override
@@ -45,7 +56,7 @@ public class LocationService extends Service {
         if (lastAlarmTime != 0) {
             long intervalTime = System.currentTimeMillis() - lastAlarmTime;
             if (actionType.equals(Intent.ACTION_USER_PRESENT) && intervalTime < Consts.ALARM_INTERVAL_MILLIS) {
-                return super.onStartCommand(intent, flags, startId);
+                return START_REDELIVER_INTENT;
             }
         }
 
@@ -53,7 +64,7 @@ public class LocationService extends Service {
         mPrefEditer.putLong(Consts.LAST_ALARM_TIME_KEY_SP, System.currentTimeMillis());
         mPrefEditer.commit();
 
-        LocationManager.getInstance(LocationService.this, new LocationHandler(this, actionType)).start();
+        LocationManager.getInstance(LocationService.this, mLocationHandler).start();
         // 添加定时器
         mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         mStartServiceIntent = new Intent(LocationService.this, StartLocationServiceReceiver.class);
@@ -64,14 +75,48 @@ public class LocationService extends Service {
         mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
                 SystemClock.elapsedRealtime() + Consts.ALARM_INTERVAL_MILLIS, Consts.ALARM_INTERVAL_MILLIS, mPi);
 
-        return super.onStartCommand(intent, flags, startId);
+        return START_REDELIVER_INTENT;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        unregisterReceiver(mReceiver);
         Log.e(TAG, "onDestroy()!");
         mAlarmManager.cancel(mPi);
     }
 
+    private void serviceDestroied() {
+        AVObject serviceDestroyObj = new AVObject("ServiceDestroy");
+        serviceDestroyObj.put("location", "");
+    }
+
+    private static class LocationHandler extends Handler {
+
+        private Context mContext;
+
+        public LocationHandler(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            switch (msg.what) {
+                case 1:
+                    String mtype = android.os.Build.MODEL; // 手机型号
+                    String mtyb = android.os.Build.BRAND;//手机品牌
+                    AVObject result = new AVObject("Location");
+                    result.put("MODEL", mtype);
+                    result.put("BRAND", mtyb);
+                    result.put("Location", (String) msg.obj);
+                    result.saveInBackground();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
 }
